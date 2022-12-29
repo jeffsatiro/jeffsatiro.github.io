7e8dfd146b63aabc057cfd3e1ca411874606d7e8
Jeferson Silva <jeferson.i.silva@gmail.com>
2022-12-29 14:02:13 -0300

    Demo menu features

--- demo/demo-menu.js
+++ demo/demo-menu.js
@@ -3,18 +3,37 @@ window.addEventListener('DOMContentLoaded', (event) => {
     innerHTML: `<div style="display:flex; gap:10px; position:fixed; bottom:15px; right:15px; z-index:999!important;">
       <v-btn icon="mdi-xml" :href="sourceUrl" target="_blank"></v-btn>
 
-      <v-menu>
+      <v-menu :close-on-content-click="false">
         <template #activator="{ props }">
           <v-btn v-bind="props" color="primary" icon="mdi-menu"></v-btn>
         </template>
 
-        <v-list width="200" class="mb-3">
-          <template v-for="(item, itemIndex) in items">
-            <v-list-item :key="itemIndex" :href="item.href">
-              {{ item.name }}
-            </v-list-item>
-          </template>
-        </v-list>
+        
+        <v-card width="300" class="mb-3">
+          <div class="d-flex flex-column">
+            <div>
+              <v-text-field
+                v-model="filter"
+                label="Filtro"
+                :hide-details="true"
+              ></v-text-field>
+            </div>
+            <v-divider></v-divider>
+            <div style="max-height:80vh; overflow:auto;">
+              <v-list>
+                <template v-for="(_item, i) in itemsFiltered()">
+                  <v-list-item
+                    :key="i"
+                    :href="\`../\${_item.id}/index.html\`"
+                    :prepend-icon="_item.icon"
+                  >
+                    {{ _item.name }}
+                  </v-list-item>
+                </template>
+              </v-list>
+            </div>
+          </div>
+        </v-card>
       </v-menu>
     </div>`,
   });
@@ -22,20 +41,78 @@ window.addEventListener('DOMContentLoaded', (event) => {
   document.body.appendChild(demoMenu);
 
   Vue.createApp({
+    methods: {
+      itemsFiltered() {
+        let filter = this.filter.toLowerCase();
+        if (!filter) return this.items;
+        return this.items.filter(item => {
+          return JSON.stringify(item).toLowerCase().includes(filter);
+        });
+      },
+    },
     data: () => ({
+      filter: '',
       items: [
-        // {name:'BrainJS', href:'../brainjs/index.html'},
-        // {name:'Championship', href:'../championship/index.html'},
-        {name:'Clockify', href:'../clockify/index.html'},
-        {name:'Diff Tool', href:'../diff-tool/index.html'},
-        {name:'Filesize Generator', href:'../filesize-generator/index.html'},
-        {name:'Laramake', href:'../laramake/index.html'},
-        {name:'Leaflet', href:'../leaflet/index.html'},
-        {name:'Lotto Simulation', href:'../lotto/index.html'},
-        // {name:'OpenAPI', href:'../openapi/index.html'},
-        {name:'QR Code', href:'../qrcode/index.html'},
-        {name:'Text tools', href:'../text/index.html'},
-        {name:'Gerador link whatsapp', href:'../whatsapp/index.html'},
+        // {
+        //   id: 'brainjs',
+        //   name: 'BrainJS',
+        //   icon: 'mdi-check',
+        // },
+        // {
+        //   id: 'championship',
+        //   name: 'Championship',
+        //   icon: 'mdi-check',
+        // },
+        {
+          id: 'clockify',
+          name: 'Clockify',
+          icon: 'mdi-alarm-check',
+        },
+        {
+          id: 'diff-tool',
+          name: 'Ferramenta diff',
+          icon: 'mdi-set-left',
+        },
+        {
+          id: 'filesize-generator',
+          name: 'Gerador de arquivo',
+          icon: 'mdi-file-download-outline',
+        },
+        {
+          id: 'laramake',
+          name: 'Gerador Laravel',
+          icon: 'mdi-laravel',
+        },
+        {
+          id: 'leaflet',
+          name: 'Mapa',
+          icon: 'mdi-map-search',
+        },
+        {
+          id: 'lotto',
+          name: 'Loteria',
+          icon: 'mdi-slot-machine',
+        },
+        // {
+        //   id: 'openapi',
+        //   name: 'OpenAPI',
+        //   icon: 'mdi-check',
+        // },
+        {
+          id: 'qrcode',
+          name: 'QR Code',
+          icon: 'mdi-qrcode',
+        },
+        {
+          id: 'text',
+          name: 'Ferramentas de texto',
+          icon: 'mdi-text-box-edit-outline',
+        },
+        {
+          id: 'whatsapp',
+          name: 'Gerador link whatsapp',
+          icon: 'mdi-whatsapp',
+        },
       ],
     }),
     computed: {
